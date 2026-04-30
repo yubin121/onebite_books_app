c0f78c1784489cb168c40cb5e7d17ac3ec6a2fa1
Yubin <yubin045@naver.com>
2026-04-30 16:29:12 +0900

    Feat: 기본 라우팅 설정

---
+++ src/app/search/page.tsx
@@ -0,0 +1,9 @@
+export default async function Page({
+  searchParams,
+}: {
+  searchParams: Promise<{ q: string }>;
+}) {
+  const { q } = await searchParams;
+
+  return <div>Search 페이지 : {q}</div>;
+}
