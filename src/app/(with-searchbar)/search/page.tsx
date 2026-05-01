5da0a15e648baf8f4fe7f5328498e92c845c02ce
Yubin <yubin045@naver.com>
2026-05-01 17:19:03 +0900

    Feat: 검색 페이지 데이터 페칭 구현

--- src/app/(with-searchbar)/search/page.tsx
+++ src/app/(with-searchbar)/search/page.tsx
@@ -1,5 +1,6 @@
-import books from '@/mock/books.json';
+// import books from '@/mock/books.json';
 import BookItem from '@/components/book-item';
+import { BookData } from '@/types';
 
 export default async function Page({
   searchParams,
@@ -7,6 +8,15 @@ export default async function Page({
   searchParams: Promise<{ q?: string }>;
 }) {
   const { q } = await searchParams;
+
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/search?q=${q}`,
+  );
+  if (!response.ok) {
+    return <div>오류가 발생했습니다...</div>;
+  }
+
+  const books: BookData[] = await response.json();
 
   return (
     <div>
