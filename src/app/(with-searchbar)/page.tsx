aa0e0e83a09511bed2bfb88f18605cea9d1eedbf
Yubin <yubin045@naver.com>
2026-05-01 21:49:14 +0900

    Fix: 캐시 설정

--- src/app/(with-searchbar)/page.tsx
+++ src/app/(with-searchbar)/page.tsx
@@ -6,7 +6,7 @@ import { BookData } from '@/types';
 async function AllBooks() {
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`,
-    { cache: 'no-store' },
+    { cache: 'force-cache' },
   );
   if (!response.ok) {
     return <div>오류가 발생했습니다...</div>;
