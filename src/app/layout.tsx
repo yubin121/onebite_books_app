04731b8906b65672bc3ef67fc9c60a11587012f2
Yubin <yubin045@naver.com>
2026-05-01 17:52:56 +0900

    Fix: 푸터에 도서 개수 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,25 @@
-import "./globals.css";
-import Link from "next/link";
-import style from "./layout.module.css";
+import './globals.css';
+import Link from 'next/link';
+import style from './layout.module.css';
+import { BookData } from '@/types';
+
+async function Footer() {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`,
+  );
+  if (!response.ok) {
+    return <footer>제작 @yubin121</footer>;
+  }
+  const books: BookData[] = await response.json();
+  const bookCount = books.length;
+
+  return (
+    <footer>
+      <div>제작 @yubin121</div>
+      <div>{bookCount}개의 도서가 등록되어 있습니다.</div>
+    </footer>
+  );
+}
 
 export default function RootLayout({
   children,
@@ -8,14 +27,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body>
         <div className={style.container}>
           <header>
-            <Link href={"/"}>📚 ONEBITE BOOKS</Link>
+            <Link href={'/'}>📚 ONEBITE BOOKS</Link>
           </header>
           <main>{children}</main>
-          <footer>제작 @winterlood</footer>
+          <Footer />
         </div>
       </body>
     </html>
