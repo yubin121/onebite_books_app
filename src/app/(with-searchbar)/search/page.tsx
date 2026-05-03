e9bdcdaa430e17d42a4df2a126152e4331760a95
Yubin <yubin045@naver.com>
2026-05-03 21:50:12 +0900

    Fix: 캐시 설정

--- src/app/(with-searchbar)/search/page.tsx
+++ src/app/(with-searchbar)/search/page.tsx
@@ -11,6 +11,7 @@ export default async function Page({
 
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/search?q=${q}`,
+    { cache: 'force-cache' },
   );
   if (!response.ok) {
     return <div>오류가 발생했습니다...</div>;
