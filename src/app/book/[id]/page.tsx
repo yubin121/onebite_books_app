005764f2cf7021938a3f77a93d2a67dddb5e6cbf
Yubin <yubin045@naver.com>
2026-05-03 21:51:01 +0900

    Fix: 동적 경로에 풀 라우트 캐시 적용

--- src/app/book/[id]/page.tsx
+++ src/app/book/[id]/page.tsx
@@ -1,4 +1,11 @@
+import { notFound } from 'next/navigation';
 import style from './page.module.css';
+
+// export const dynamicParams = false;
+
+export function generateStaticParams() {
+  return [{ id: '1' }, { id: '2' }, { id: '3' }];
+}
 
 export default async function Page({
   params,
@@ -11,6 +18,9 @@ export default async function Page({
     `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/${id}`,
   );
   if (!response.ok) {
+    if (response.status === 404) {
+      notFound();
+    }
     return <div>오류가 발생했습니다...</div>;
   }
 
