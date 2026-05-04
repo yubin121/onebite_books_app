dbc21e34f2d9f1ff12af14fb34c76c0a766092b4
Yubin <yubin045@naver.com>
2026-05-04 19:09:08 +0900

    Feat: 에러 페이지 구현

---
+++ src/app/book/[id]/error.tsx
@@ -0,0 +1,34 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+import { startTransition, useEffect } from 'react';
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error;
+  reset: () => void;
+}) {
+  const router = useRouter();
+
+  useEffect(() => {
+    console.error(error.message);
+  }, [error]);
+
+  return (
+    <div>
+      <h3>오류가 발생했습니다.</h3>
+      <button
+        onClick={() => {
+          startTransition(() => {
+            router.refresh(); // 현재 페이지에 필요한 서버 컴포넌트들을 다시 불러옴 (Next 서버에 재요청)
+            reset(); // 에러 상태를 초기화, 컴포넌트들을 다시 렌더링
+          });
+        }}
+      >
+        재시도
+      </button>
+    </div>
+  );
+}
