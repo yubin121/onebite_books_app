b7ef9a920e2868f4347a888d2f5b89b3d07cad42
Yubin <yubin045@naver.com>
2026-05-05 20:04:15 +0900

    Fix: 모든 상세 페이지를 Static 페이지로 수정

--- src/app/book/[id]/page.tsx
+++ src/app/book/[id]/page.tsx
@@ -8,8 +8,19 @@ import type { Metadata } from 'next';
 
 // export const dynamicParams = false;
 
-export function generateStaticParams() {
-  return [{ id: '1' }, { id: '2' }, { id: '3' }];
+export async function generateStaticParams() {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`,
+  );
+  if (!response.ok) {
+    throw new Error(response.statusText);
+  }
+
+  const books: BookData[] = await response.json();
+
+  return books.map((book) => ({
+    id: book.id.toString(),
+  }));
 }
 
 async function BookDetail({ bookId }: { bookId: string }) {
